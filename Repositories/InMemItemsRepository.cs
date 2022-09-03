bf627dda6dbc05a43b409a0f2a657821a22acee0
Ayush <ayushrj1516@gmail.com>
2022-09-04 00:00:36 +0530

    updated

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -9,7 +9,7 @@ namespace Catalog.Repositories
     {
         private readonly List<Item> items = new()
         {
-            new Item{Id = Guid.NewGuid(),Name = "Potion",Price = 9,CreatedDate = DateTimeOffset.UtcNow},
+            new Item{Id = Guid.NewGuid(),Name = "Portion",Price = 9,CreatedDate = DateTimeOffset.UtcNow},
             new Item{Id = Guid.NewGuid(),Name = "Iron Sword",Price = 20,CreatedDate = DateTimeOffset.UtcNow},
             new Item{Id = Guid.NewGuid(),Name = "Bronze Shield",Price = 18,CreatedDate = DateTimeOffset.UtcNow},
         };
