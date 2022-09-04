b862ff2d8f9fe918e25e2bf771d3d90a1458154d
Ayush <ayushrj1516@gmail.com>
2022-09-04 22:58:05 +0530

    Update ItemsController.cs

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,11 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Repositories;
+using Catalog.Entities;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace Catalog.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("items")]
     public class ItemsController : ControllerBase
     {
-        
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController(){
+            repository = new InMemItemsRepository();
+
+        }
+
+        //HTTPGET tells when someone do GET/items, down method is going to react
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+           var items = repository.GetItems();
+           return items;
+        }
+
+        //GET/items/{id}
+        [HttpGet("{id}")]
+        public Item GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            return item;
+        }
+
     }
 }
