11d1d0baa4f007bc21fb422f4ef5689a2e0ccc4e
Ayush <ayushrj1516@gmail.com>
2022-09-08 01:43:06 +0530

    mongodbrepository completion

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
-using Catalog.Entities; 
+using Catalog.Entities;
+using MongoDB.Bson; 
 using MongoDB.Driver;
 
 namespace Catalog.Repositories
@@ -10,6 +11,8 @@ namespace Catalog.Repositories
         private const string databaseName = "catalog";
         private const string collectionName = "items";
         private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+        //we will use this to filter the returned items feom collection
 
         public MongoDbItemsRepository(IMongoClient mongoClient)
         {
@@ -24,23 +27,26 @@ namespace Catalog.Repositories
 
         public void DeleteItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id,id);
+            itemsCollection.DeleteOne(filter);
         }
 
         public Item GetItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id,id);
+            return itemsCollection.Find(filter).SingleOrDefault();
         }
 
 
         public IEnumerable<Item> GetItems()
         {
-            throw new NotImplementedException();
+            return itemsCollection.Find(new BsonDocument()).ToList();
         }
 
         public void UpdateItem(Item item)
         {
-            throw new NotImplementedException();   
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id,item.Id);
+            itemsCollection.ReplaceOne(filter,item);
         }
     }
     
